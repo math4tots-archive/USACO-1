7e2ebc964bba81615cf355c5f3ccad8aa442ea38
Jen K <toto4tots@Jennifers-MacBook-Pro-3.local>
2018-12-26 12:35:58 -0800

    more attempts on milk2

--- milk2/main.py
+++ milk2/main.py
@@ -1,43 +1,48 @@
-'''
-ID: jennife29
-LANG: PYTHON3
-TASK: milk2
-'''
+def compare(times):
+    from operator import itemgetter
+    stimes = sorted(times, key=itemgetter(0))
+    cont = []
+    start = 0
+    end = 10**9
+    for i in range(len(stimes) - 1):
+        print(f"start {start} end {end}")
+        if stimes[i][1] == end:
+            if stimes[i][1] >= stimes[i + 1][0]:
+                start = stimes[i][0]
+                end = stimes[i + 1][1]
+            else:
+                cont.append([start, end])
+            continue
+        if stimes[i][1] < stimes[i + 1][0]:
+            print(f"{stimes[i]}")
+            if end != 10**9:
+                cont.append([start, end])
+            start = stimes[i + 1][0]
+            end = stimes[i + 1][1]
+
+        if stimes[i][1] >= stimes[i + 1][0]:
+            start = stimes[i][0]
+            end = stimes[i + 1][1]
+    if end not in cont:
+        cont.append([start, end])
+    return cont
 
 
-def milk_time(times):
+
+def diff(cont):
     '''
-    times = [[a, b], [a, b], [a, b]]
+    Subtract lst[1] with lst[0] and put into a list of difference.
+    Spits out the max difference
 
     '''
-    a = 300
-    b = 10**6
-    longest = [0, 0]
-    for time in times:
-        if a <= time[0] < b:
-            b = time[1]
-            longest[0] = b - a
-        else:
-            if time[0] > b:
-                longest[1] = time[0] - b
-    return " ".join(str(t) for t in longest)
-
-with open('milk2.in') as f:
-
-    N = int(f.readline())
-    times = []
-    for n in range(N):
-        farm = [int(x) for x in f.readline().strip().split()]
-        times.append(farm)
-
-
-with open("milk2.out", "w") as f:
-    f.write(f"{milk_time(times)}\n")
+    pass
 
 
 
 
-
-
-
-#Maybe I can make a max(generator)?
+times = []
+N = int(input())
+for n in range(N):
+    farm = [int(x) for x in input().split()]
+    times.append(farm)
+print(compare(times))
---
+++ milk2/old.py
@@ -0,0 +1,36 @@
+'''
+ID: jennife29
+LANG: PYTHON3
+TASK: milk2
+'''
+
+
+def milk_time(times):
+    '''
+    times = [[a, b], [a, b], [a, b]]
+
+    '''
+    a = 300
+    b = 10**6
+    longest = [0, 0]
+    for time in times:
+        if a <= time[0] < b:
+            b = time[1]
+            longest[0] = b - a
+        else:
+            if time[0] > b:
+                longest[1] = time[0] - b
+    return " ".join(str(t) for t in longest)
+
+with open('milk2.in') as f:
+
+    N = int(f.readline())
+    times = []
+    for n in range(N):
+        farm = [int(x) for x in f.readline().strip().split()]
+        times.append(farm)
+
+
+with open("milk2.out", "w") as f:
+    f.write(f"{milk_time(times)}\n")
+

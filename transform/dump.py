bb2d5953fb3d499d9c5fd7672a560f8277c98ff9
Jen K <toto4tots@Jennifers-MacBook-Pro-3.local>
2019-01-07 20:41:47 -0800

    started transform

---
+++ transform/dump.py
@@ -0,0 +1,9 @@
+row = [[1, 2, 3], [4, 5, 1], [2, 5, 0]]
+print(list(reversed(row)))
+
+
+
+new = [[], []]
+for i in range(2):
+    new[i].append(i)
+print(new)
---
+++ transform/main.py
@@ -0,0 +1,73 @@
+def convert():
+    '''
+    Takes '@' and converts it to 1
+    Takes '_' and converts it to 0
+    '''
+    pass
+
+
+def lst_maker(N):
+    '''
+    Creates an empty list of N lists
+
+    '''
+    lst = []
+    for x in range(N):
+        lst.append([])
+    return lst
+
+def ninetydegrees(pattern, N):
+    '''
+    Pattern is a list of lists
+    N is the number of rows/columns
+
+    Returns a list of lists that have been transformed
+    90 degrees
+    '''
+    temp = lst_maker(N)
+    for row in pattern:
+        for i in range(len(row)):
+            temp[i].append(row[i])
+    new = []
+    for t in temp:
+        new.append(t[::-1])
+    return new
+
+
+def one_eighty(pattern, N):
+    '''
+    Does an additional 90 degrees to ninetydegrees(pattern, N)
+    '''
+    return ninetydegrees(ninetydegrees(pattern, N), N)
+
+def two_seventy(pattern, N):
+    return ninetydegrees(one_eighty(pattern, N), N)
+
+def reflection(pattern, N):
+
+    #CHECK THIS!!!
+    return list(reversed(pattern))
+
+
+
+def check(pattern, real, N):
+    '''
+    Checks to see if any of the combination creates
+    an identical image as the real image.
+    If so, return the corresponding pattern number
+    Else, return invalid transtormation (#7)
+
+    '''
+    pass
+
+
+
+
+
+
+
+
+
+
+
+

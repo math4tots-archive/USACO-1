97e0e804aa0e362a114b2b3d6106382862ac2010
Jen K <toto4tots@Jennifers-MacBook-Pro-3.local>
2019-01-08 22:21:25 -0800

    more work on transform

--- transform/main.py
+++ transform/main.py
@@ -1,9 +1,19 @@
-def convert():
+def convert(chara):
     '''
     Takes '@' and converts it to 1
-    Takes '_' and converts it to 0
+    Takes '-' and converts it to 0
     '''
     pass
+
+
+def check(pattern, real, N):
+    '''
+    if the new pattern and the real pattern are the same then it
+    returns True. Else, it will return False
+
+    '''
+    if pattern == real:
+        return True
 
 
 def lst_maker(N):
@@ -21,8 +31,12 @@ def ninetydegrees(pattern, N):
     Pattern is a list of lists
     N is the number of rows/columns
 
-    Returns a list of lists that have been transformed
+    Creates list of lists that have been transformed
     90 degrees
+
+    Returns T if this pattern is the same as the real product
+
+
     '''
     temp = lst_maker(N)
     for row in pattern:
@@ -31,6 +45,7 @@ def ninetydegrees(pattern, N):
     new = []
     for t in temp:
         new.append(t[::-1])
+
     return new
 
 
@@ -49,7 +64,30 @@ def reflection(pattern, N):
 
 # assert reflection([[1, 0, 1], [0, 0, 0], [1, 1, 0]], 3) == [[1, 1, 0], [0, 0, 0], [1, 0, 1]], reflection([[1, 0, 1], [0, 0, 0], [1, 1, 0]], 3)
 
-def check(pattern, real, N):
+def combination(pattern, N):
+    '''
+    It takes the pattern and reflects it horizontally. Then it returns
+    a new pattern that has been subjected to other transformation
+
+    However, problem- There are three possible patterns that combination
+    may produce.
+
+    IDEA:
+    Have a list that contains all of that. When at solve function, split this up
+    specifically for combination
+        -) Doesn't feel very efficient
+
+    '''
+    all_patterns = []
+    new = reflection(pattern, N)
+    all_patterns.append(ninetydegrees(pattern, N))
+    all_patterns.append(one_eighty(pattern, N))
+    all_patterns.append(two_seventy(pattern, N))
+
+    return all_patterns
+
+
+def solve(pattern, real, N):
     '''
     Checks to see if any of the combination creates
     an identical image as the real image.
@@ -57,7 +95,39 @@ def check(pattern, real, N):
     Else, return invalid transtormation (#7)
 
     '''
-    pass
+    if pattern == real:
+        return 6
+    if check(ninetydegrees(pattern, N), real, N):
+        return 1
+    if check(one_eighty(pattern, N), real, N):
+        return 2
+    if check(two_seventy(pattern, N), real, N):
+        return 3
+    if check(reflection(pattern, N), real, N):
+        return 4
+    for pat in combination(pattern, N):
+        if check(pat, real, N):
+            return 5
+    return 7
+
+
+# THE FOLLOWING ARE PRODUCING BUGS!!
+assert solve([[1, 0, 1]], [[0, 1, 1]], 1) == 7
+assert solve([[1, 0, 0, 1], [1, 1, 0, 1], [1, 1, 1, 1], [0, 1, 0, 1]], [[1, 0, 0, 1], [1, 0, 1, 1], [1, 1, 1, 1], [1, 0, 1, 0]], 4) == 4, solve([[1, 0, 0, 1], [1, 1, 0, 1], [1, 1, 1, 1], [0, 1, 0, 1]], [[1, 0, 0, 1], [1, 0, 1, 1], [1, 1, 1, 1], [1, 0, 1, 0]], 4)
+
+
+
+
+
+
+
+# assert solve([[1, 0, 0, 1], [1, 1, 0, 1], [1, 1, 1, 1], [0, 1, 0, 1]], [[1, 0, 1, 0], [1, 1, 1, 1], [1, 0, 1, 1], [1, 0, 0, 1]], 4) == 2, solve([[1, 0, 0, 1], [1, 1, 0, 1], [1, 1, 1, 1], [0, 1, 0, 1]], [[1, 0, 1, 0], [1, 1, 1, 1], [1, 0, 1, 1], [1, 0, 0, 1]], 4)
+# assert solve([[0]], [[0]], 1) == 7, solve([[0]], [[0]], 1)
+# assert solve([[1, 0, 1], [0, 0, 0], [1, 1, 0]], [[1, 0, 1], [1, 0, 0], [0, 0, 1]], 3) == 1, solve([[1, 0, 1], [0, 0, 0], [1, 1, 0]], [[1, 0, 1], [1, 0, 0], [0, 0, 1]], 3)
+# assert solve([[1, 0, 1], [0, 0, 0], [1, 1, 0]], [[1, 1, 0], [0, 0, 0], [1, 0, 1]], 3) == 4, solve([[1, 0, 1], [0, 0, 0], [1, 1, 0]], [[1, 1, 0], [0, 0, 0], [1, 0, 1]], 3)
+
+
+
 
 
 
